4c122bfa85be57f729bd05fe9a9330c7f55da323
MsEmily1020 <peace10200khs@gmail.com>
2023-08-24 19:02:15 +0900

    🎉 프로젝트 초기 셋팅

---
+++ src/main/java/com/share/share_scripts/ShareScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.share.share_scripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareScriptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareScriptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/share/share_scripts/ShareScriptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.share.share_scripts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareScriptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
