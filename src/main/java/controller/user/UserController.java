55e2172bb36eb39fac8a7a753eab6a3563c7b125
MsEmily1020 <peace10200khs@gmail.com>
2023-08-31 19:34:50 +0900

    ✨ user controller getmapping 기능 구현

--- src/main/java/controller/user/UserController.java
+++ src/main/java/controller/user/UserController.java
@@ -2,13 +2,17 @@ package controller.user;
 
 import domain.user.User;
 import dto.user.AddUserRequest;
+import dto.user.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import service.user.UserService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,6 +25,17 @@ public class UserController {
         User savedUser = userService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedUser);
+    }
+
+    // read
+    @GetMapping("/api/users")
+    public ResponseEntity<List<UserResponse>> findAllUsers() {
+        List<UserResponse> users = userService.findAll()
+                .stream()
+                .map(UserResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(users);
     }
 
 
