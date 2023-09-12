5361deeae1cf9ef25e15c1c3e00a423e043a5d2a
MsEmily1020 <peace10200khs@gmail.com>
2023-09-13 08:13:27 +0900

    ✨ favorite controller - putmapping

--- src/main/java/com/share/share_scripts/controller/favorite/FavoriteController.java
+++ src/main/java/com/share/share_scripts/controller/favorite/FavoriteController.java
@@ -3,16 +3,14 @@ package com.share.share_scripts.controller.favorite;
 import com.share.share_scripts.domain.favorite.Favorite;
 import com.share.share_scripts.dto.favorite.AddFavoriteRequest;
 import com.share.share_scripts.dto.favorite.FavoriteResponse;
+import com.share.share_scripts.dto.favorite.UpdateFavoriteRequest;
 import com.share.share_scripts.service.favorite.FavoriteService;
 import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +35,14 @@ public class FavoriteController {
                 .toList();
 
         return ResponseEntity.ok().body(favorite);
+    }
+
+    @PutMapping("/api/favorite/{id}")
+    public ResponseEntity<Favorite> updateFavorite(@PathVariable Long id,
+                                                   @RequestBody UpdateFavoriteRequest request) {
+        Favorite updatedFavorite = favoriteService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedFavorite);
     }
 }
