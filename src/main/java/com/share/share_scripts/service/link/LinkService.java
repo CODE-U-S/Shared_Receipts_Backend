9f97c872776619f52bfb760c3b72e7fc7713582f
MsEmily1020 <peace10200khs@gmail.com>
2023-09-12 19:26:37 +0900

    ✨ link service - save

---
+++ src/main/java/com/share/share_scripts/service/link/LinkService.java
@@ -0,0 +1,19 @@
+package com.share.share_scripts.service.link;
+
+import com.share.share_scripts.domain.link.Link;
+import com.share.share_scripts.dto.link.AddLinkRequest;
+import com.share.share_scripts.dto.post.PostResponse;
+import com.share.share_scripts.repository.link.LinkRepository;
+import com.share.share_scripts.repository.post.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LinkService {
+    private LinkRepository linkRepository;
+
+    public Link save(AddLinkRequest request) {
+        return linkRepository.save(request.toEntity());
+    }
+}
