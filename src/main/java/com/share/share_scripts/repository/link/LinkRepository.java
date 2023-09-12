698359ea0808d949965bd8b64f1776b666bb47df
MsEmily1020 <peace10200khs@gmail.com>
2023-09-12 19:24:56 +0900

    ✨ link repository 생성

---
+++ src/main/java/com/share/share_scripts/repository/link/LinkRepository.java
@@ -0,0 +1,7 @@
+package com.share.share_scripts.repository.link;
+
+import com.share.share_scripts.domain.link.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+}
