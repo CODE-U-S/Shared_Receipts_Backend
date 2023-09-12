54d168cade848bf5979fdd6236ceb9c48798e63e
MsEmily1020 <peace10200khs@gmail.com>
2023-09-12 19:30:50 +0900

    ✨ link service - update

--- src/main/java/com/share/share_scripts/service/link/LinkService.java
+++ src/main/java/com/share/share_scripts/service/link/LinkService.java
@@ -2,9 +2,11 @@ package com.share.share_scripts.service.link;
 
 import com.share.share_scripts.domain.link.Link;
 import com.share.share_scripts.dto.link.AddLinkRequest;
+import com.share.share_scripts.dto.link.UpdateLinkRequest;
 import com.share.share_scripts.dto.post.PostResponse;
 import com.share.share_scripts.repository.link.LinkRepository;
 import com.share.share_scripts.repository.post.PostRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,4 +24,15 @@ public class LinkService {
     public List<Link> findAll() { return linkRepository.findAll(); }
 
     public void delete(Long id) { linkRepository.deleteById(id); }
+
+    @Transactional
+    public Link update(Long id, UpdateLinkRequest request) {
+        Link link = linkRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id ));
+
+        link.update(request.getPostNo(),
+                request.getUrl());
+
+        return link;
+    }
 }
