9bc0f6ee312bea45844d5e47d8da85165364d65c
MsEmily1020 <peace10200khs@gmail.com>
2023-09-12 19:35:01 +0900

    ✨ link controller - putmapping

--- src/main/java/com/share/share_scripts/controller/link/LinkController.java
+++ src/main/java/com/share/share_scripts/controller/link/LinkController.java
@@ -3,14 +3,13 @@ package com.share.share_scripts.controller.link;
 import com.share.share_scripts.domain.link.Link;
 import com.share.share_scripts.dto.link.AddLinkRequest;
 import com.share.share_scripts.dto.link.LinkResponse;
+import com.share.share_scripts.dto.link.UpdateLinkRequest;
 import com.share.share_scripts.service.link.LinkService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.sql.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +34,14 @@ public class LinkController {
                 .toList();
 
         return ResponseEntity.ok().body(link);
+    }
+
+    @PutMapping("/api/post/{id}")
+    public ResponseEntity<Link> updateLink(@PathVariable Long id,
+                                           @RequestBody UpdateLinkRequest request) {
+        Link updatedLink = linkService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedLink);
     }
 }
