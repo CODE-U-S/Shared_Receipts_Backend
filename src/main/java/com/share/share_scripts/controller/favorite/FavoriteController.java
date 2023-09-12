ff2b2fabd5d23c3c5c2a4805011dca77dfea0b3f
MsEmily1020 <peace10200khs@gmail.com>
2023-09-13 08:12:10 +0900

    ✨ favorite controller - getmapping

--- src/main/java/com/share/share_scripts/controller/favorite/FavoriteController.java
+++ src/main/java/com/share/share_scripts/controller/favorite/FavoriteController.java
@@ -2,14 +2,19 @@ package com.share.share_scripts.controller.favorite;
 
 import com.share.share_scripts.domain.favorite.Favorite;
 import com.share.share_scripts.dto.favorite.AddFavoriteRequest;
+import com.share.share_scripts.dto.favorite.FavoriteResponse;
 import com.share.share_scripts.service.favorite.FavoriteService;
+import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +27,15 @@ public class FavoriteController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedFavorite);
+    }
+
+    @GetMapping("/api/favorite")
+    public ResponseEntity<List<FavoriteResponse>> findAllFavorite() {
+        List<FavoriteResponse> favorite = favoriteService.findAll()
+                .stream()
+                .map(FavoriteResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(favorite);
     }
 }
