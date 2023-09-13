f8ea6511a0124cd5f48c302c1c4d12205c936e1a
MsEmily1020 <peace10200khs@gmail.com>
2023-09-13 09:03:11 +0900

    ✨ update receipt request

--- src/main/java/com/share/share_scripts/dto/receipt/UpdateReceiptRequest.java
+++ src/main/java/com/share/share_scripts/dto/receipt/UpdateReceiptRequest.java
@@ -1,4 +1,15 @@
 package com.share.share_scripts.dto.receipt;
 
+import com.share.share_scripts.domain.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class UpdateReceiptRequest {
+    private Post postNo;
+    private String name;
+    private Integer price;
 }
