e978239f3afd1951d4c8571ecca3527db2f332ac
MsEmily1020 <peace10200khs@gmail.com>
2023-09-13 09:02:25 +0900

    ✨ receipt response

--- src/main/java/com/share/share_scripts/dto/receipt/ReceiptResponse.java
+++ src/main/java/com/share/share_scripts/dto/receipt/ReceiptResponse.java
@@ -1,4 +1,20 @@
 package com.share.share_scripts.dto.receipt;
 
+import com.share.share_scripts.domain.post.Post;
+import com.share.share_scripts.domain.receipt.Receipt;
+import lombok.Getter;
+
+@Getter
 public class ReceiptResponse {
+    private Long receiptNo;
+    private Post postNo;
+    private String name;
+    private Integer price;
+
+    public ReceiptResponse(Receipt receipt) {
+        this.receiptNo = receipt.getReceiptNo();
+        this.postNo = receipt.getPostNo();
+        this.name = receipt.getName();
+        this.price = receipt.getPrice();
+    }
 }
