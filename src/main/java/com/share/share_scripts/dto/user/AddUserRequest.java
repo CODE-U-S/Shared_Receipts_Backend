104566190f4d369d822bab5b924bc9ead2e52413
MsEmily1020 <peace10200khs@gmail.com>
2023-12-21 12:57:36 +0900

    🔥 message 삭제

--- src/main/java/com/share/share_scripts/dto/user/AddUserRequest.java
+++ src/main/java/com/share/share_scripts/dto/user/AddUserRequest.java
@@ -14,33 +14,30 @@ import java.util.Date;
 @Getter
 @Setter
 public class AddUserRequest {
-    @NotBlank(message = "사용자 이름은 필수값 입니다.")
+    @NotBlank
     private String userName;
 
-    @NotBlank(message = "사용자 아이디는 필수값 입니다.")
+    @NotBlank
     private String userId;
 
-    @NotBlank(message = "사용자 비밀번호는 필수값 입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 16자의 비밀번호여야 합니다.")
+    @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}")
     private String userPw;
 
-    @NotBlank(message = "사용자 이메일은 필수값 입니다.")
-    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank
+    @Email
     private String userEmail;
 
-    @NotBlank(message = "사용자 전화번호는 필수값 입니다.")
-    @Pattern(regexp = "(0\\d{1,2})-(\\d{3,4})-(\\d{4})",
-            message = "전화번호 형식이 아닙니다.")
+    @NotBlank
+    @Pattern(regexp = "(0\\d{1,2})-(\\d{3,4})-(\\d{4})")
     private String userPhone;
 
-    @NotBlank(message = "사용자 생년월일은 필수값 입니다.")
-    @Pattern(regexp = "\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])",
-            message = "생년월일 형식이 아닙니다.")
-    @PastOrPresent(message = "미래값은 들어갈 수 없습니다.")
+    @NotBlank
+    @Pattern(regexp = "\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])")
+    @PastOrPresent
     private Date userBirth;
 
-    @NotNull(message = "사용자 성별은 필수값 입니다.")
+    @NotNull
     private Integer userGender;
 
     private String userImg;
