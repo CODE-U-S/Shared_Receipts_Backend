7c6af57973771a05e879d0db0d8cfe33e07ff73f
MsEmily1020 <peace10200khs@gmail.com>
2023-12-25 21:11:04 +0900

    ✨ receiptService에 BindingResultException 상속화

--- src/main/java/com/share/share_scripts/service/receipt/ReceiptService.java
+++ src/main/java/com/share/share_scripts/service/receipt/ReceiptService.java
@@ -5,18 +5,22 @@ import com.share.share_scripts.dto.post.AddPostRequest;
 import com.share.share_scripts.dto.receipt.AddReceiptRequest;
 import com.share.share_scripts.dto.receipt.UpdateReceiptRequest;
 import com.share.share_scripts.repository.receipt.ReceiptRepository;
+import com.share.share_scripts.service.BindingResultException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-public class ReceiptService {
+public class ReceiptService extends BindingResultException {
     private final ReceiptRepository receiptRepository;
 
-    public Receipt save(AddReceiptRequest request) {
+    public Receipt save(AddReceiptRequest request, BindingResult bindingResult) {
+        badRequestException(bindingResult);
+
         return receiptRepository.save(request.toEntity());
     }
 
@@ -25,7 +29,9 @@ public class ReceiptService {
     public void delete(Long id) { receiptRepository.deleteById(id); }
 
     @Transactional
-    public Receipt update(Long id, UpdateReceiptRequest request) {
+    public Receipt update(Long id, UpdateReceiptRequest request, BindingResult bindingResult) {
+        badRequestException(bindingResult);
+
         Receipt receipt = receiptRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
