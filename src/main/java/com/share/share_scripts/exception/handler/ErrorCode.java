2d8badd6bc97dbe9439ea07621a655f8e6859770
MsEmily1020 <peace10200khs@gmail.com>
2023-12-25 16:59:07 +0900

    ✨ 전체적인 예외 작성(400, 500 에러)

--- src/main/java/com/share/share_scripts/exception/handler/ErrorCode.java
+++ src/main/java/com/share/share_scripts/exception/handler/ErrorCode.java
@@ -7,7 +7,35 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    ID_DUPLICATE(HttpStatus.CONFLICT, "중복된 아이디입니다.");
+    /**
+     * 400 BAD_REQUEST : 잘못된 요청
+     */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    /**
+     * 404 NOT_FOUND : 정보를 찾을 수 없음.
+     *
+     * 1. POSTS : 게시글
+     * 2. USERS : 사용자
+     */
+    POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
+    USERS_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
+
+
+    /**
+     * 405 METHOD_NOT_ALLOWED : 허용되지 않은 Request Method 호출
+     */
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메소드입니다."),
+
+    /**
+     * 409 ID_DUPLICATE : 중복된 아이디 요청
+     */
+    ID_DUPLICATE(HttpStatus.CONFLICT, "중복된 아이디입니다."),
+
+    /**
+     * 500 INTERNAL_SERVER_ERROR : 내부 서버 오류
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다.");
 
     private final HttpStatus status;
     private final String message;
