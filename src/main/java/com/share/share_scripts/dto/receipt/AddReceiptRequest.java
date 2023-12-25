af1bb3e2b091e9f7808701c93d112c7f30b311cc
MsEmily1020 <peace10200khs@gmail.com>
2023-12-25 21:43:33 +0900

    ✨ receipt 유효성 검사 및 badRequestException 예외 처리

--- src/main/java/com/share/share_scripts/dto/receipt/AddReceiptRequest.java
+++ src/main/java/com/share/share_scripts/dto/receipt/AddReceiptRequest.java
@@ -2,6 +2,8 @@ package com.share.share_scripts.dto.receipt;
 
 import com.share.share_scripts.domain.post.Post;
 import com.share.share_scripts.domain.receipt.Receipt;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,11 @@ import lombok.NoArgsConstructor;
 @Getter
 public class AddReceiptRequest {
     private Post postNo;
+
+    @NotBlank
     private String name;
+
+    @NotNull
     private Integer price;
 
     public Receipt toEntity() {
