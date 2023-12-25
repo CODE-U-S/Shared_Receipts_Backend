5b382b6252a6b649ba7726343533642a87d98d52
MsEmily1020 <peace10200khs@gmail.com>
2023-12-25 17:47:32 +0900

    ✨ user not found 404 error exception 클래스 생성

--- src/main/java/com/share/share_scripts/exception/handler/ErrorCode.java
+++ src/main/java/com/share/share_scripts/exception/handler/ErrorCode.java
@@ -18,8 +18,8 @@ public enum ErrorCode {
      * 1. POSTS : 게시글
      * 2. USERS : 사용자
      */
-    POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
-    USERS_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
 
 
     /**
