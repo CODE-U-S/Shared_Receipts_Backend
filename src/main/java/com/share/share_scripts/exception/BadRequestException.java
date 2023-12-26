3199dc85714e0172db342ee82f68aa731c23aa24
MsEmily1020 <peace10200khs@gmail.com>
2023-12-26 21:41:07 +0900

    🚚 BadRequestException클래스로 badRequestException method move

--- src/main/java/com/share/share_scripts/exception/BadRequestException.java
+++ src/main/java/com/share/share_scripts/exception/BadRequestException.java
@@ -3,9 +3,18 @@ package com.share.share_scripts.exception;
 import com.share.share_scripts.exception.handler.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 
 @Getter
 @RequiredArgsConstructor
 public class BadRequestException extends RuntimeException {
     private final ErrorCode errorCode;
+
+    /**
+     * bad request 인지 확인 하는 메소드
+     * @param bindingResult
+     */
+    public static void badRequestException(BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) throw new BadRequestException(ErrorCode.BAD_REQUEST);
+    }
 }
