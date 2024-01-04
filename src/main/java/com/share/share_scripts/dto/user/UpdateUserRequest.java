c52d8074845fe6716ce7c34334548abc0eac0887
MsEmily1020 <peace10200khs@gmail.com>
2024-01-04 22:25:24 +0900

    ✨ user update request 유효성 검사

--- src/main/java/com/share/share_scripts/dto/user/UpdateUserRequest.java
+++ src/main/java/com/share/share_scripts/dto/user/UpdateUserRequest.java
@@ -1,5 +1,8 @@
 package com.share.share_scripts.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,15 @@ import java.util.Date;
 @AllArgsConstructor
 @Getter
 public class UpdateUserRequest {
+    @NotBlank
     private String userName;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}")
     private String userPw;
+
+    @NotNull
     private Integer userGender;
+
     private String userImg;
 }
