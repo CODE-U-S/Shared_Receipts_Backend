c2e89efdf6af4f416a74f935743b65b274add634
MsEmily1020 <peace10200khs@gmail.com>
2024-01-04 22:35:04 +0900

    ✨ favorite request 유효성 검사

--- src/main/java/com/share/share_scripts/dto/favorite/AddFavoriteRequest.java
+++ src/main/java/com/share/share_scripts/dto/favorite/AddFavoriteRequest.java
@@ -4,6 +4,7 @@ import com.share.share_scripts.domain.favorite.Favorite;
 import com.share.share_scripts.domain.link.Link;
 import com.share.share_scripts.domain.post.Post;
 import com.share.share_scripts.domain.user.User;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class AddFavoriteRequest {
+    @NotNull
     private User user;
+
+    @NotNull
     private Post post;
 
     public Favorite toEntity() {
