247b30bc55b29cfda2df07336609fdf1cbbd2fe0
MsEmily1020 <peace10200khs@gmail.com>
2024-01-04 22:31:19 +0900

    ✨ item service, controller에서 exception 제어

--- src/main/java/com/share/share_scripts/service/item/ItemService.java
+++ src/main/java/com/share/share_scripts/service/item/ItemService.java
@@ -3,19 +3,31 @@ package com.share.share_scripts.service.item;
 import com.share.share_scripts.domain.item.Item;
 import com.share.share_scripts.dto.item.AddItemRequest;
 import com.share.share_scripts.dto.item.UpdateItemRequest;
+import com.share.share_scripts.exception.ReceiptNotFoundException;
+import com.share.share_scripts.exception.handler.ErrorCode;
 import com.share.share_scripts.repository.item.ItemRepository;
+import com.share.share_scripts.repository.receipt.ReceiptRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
+
+import static com.share.share_scripts.exception.BadRequestException.badRequestException;
 
 @RequiredArgsConstructor
 @Service
 public class ItemService {
     private final ItemRepository itemRepository;
+    private final ReceiptRepository receiptRepository;
 
-    public Item save(AddItemRequest request) {
+    public Item save(AddItemRequest request, BindingResult bindingResult) {
+        badRequestException(bindingResult);
+
+        receiptRepository.findById(request.getReceipt().getReceiptNo())
+                .orElseThrow(() -> new ReceiptNotFoundException(ErrorCode.RECEIPT_NOT_FOUND));
+
         return itemRepository.save(request.toEntity());
     }
 
@@ -24,9 +36,11 @@ public class ItemService {
     public void delete(Long id) { itemRepository.deleteById(id); }
 
     @Transactional
-    public Item update(Long id, UpdateItemRequest request) {
+    public Item update(Long id, UpdateItemRequest request, BindingResult bindingResult) {
+        badRequestException(bindingResult);
+
         Item item = itemRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+                .orElseThrow(() -> new ItemNotFoundException(ErrorCode.ITEM_NOT_FOUND));
 
         item.update(
                 request.getItemName(),
